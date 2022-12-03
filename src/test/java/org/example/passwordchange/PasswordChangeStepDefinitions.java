c3a6b947503010b9f2144665b34c137dcfb42a91
Luís Filipe Soutello <lfsoutello@gmail.com>
2022-12-03 17:33:13 -0300

    fixes

--- src/test/java/org/example/passwordchange/PasswordChangeStepDefinitions.java
+++ src/test/java/org/example/passwordchange/PasswordChangeStepDefinitions.java
@@ -75,4 +75,42 @@ public class PasswordChangeStepDefinitions {
         });
         driver.quit();
     }
+
+    @Given("user fill sign in form with new data")
+    public void fillSignInFormNewData() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("http://multibags.1dt.com.br/shop/customer/registration.html");
+
+        WebElement firstName = driver.findElement(By.id("firstName"));
+        WebElement lastName = driver.findElement(By.id("lastName"));
+        WebElement state = driver.findElement(By.id("hidden_zones"));
+        WebElement email = driver.findElement(By.id("emailAddress"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement repeatPassword = driver.findElement(By.id("passwordAgain"));
+
+        String randomString = RandomStringUtils.randomAlphabetic(8);
+
+        firstName.sendKeys("Seu Zé ");
+        lastName.sendKeys("Do Teste");
+        state.sendKeys("São Paulo");
+        email.sendKeys(randomString + "seuze@test.com");
+        password.sendKeys("123456");
+        repeatPassword.sendKeys("123456");
+    }
+
+    @When("user clicks create account button")
+    public void clickCreateAccountButton() {
+        WebElement createAccountButton = driver.findElement(By.className("login-btn"));
+        createAccountButton.click();
+    }
+
+    @Then("successful login")
+    public void pageRedirect() {
+        new WebDriverWait(driver,10L).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getCurrentUrl().startsWith("http://multibags.1dt.com.br/shop/customer/dashboard.html");
+            }
+        });
+    }
 }
--- src/test/resources/org/example/passwordchange/passwordchange.feature
+++ src/test/resources/org/example/passwordchange/passwordchange.feature
@@ -1,18 +1,18 @@
 Feature: Change Password
 
   Scenario: Successful change password
-    Given user fill form with new data
-    When user clicks create account
-    Then the page is redirected to customer dashboard
+    Given user fill sign in form with new data
+    When user clicks create account button
+    Then successful login
     When user go to change password page
     And user fill password change form
     And user clicks change password
     Then change password success message is displayed
 
   Scenario: new password mismatch
-    Given user fill form with new data
-    When user clicks create account
-    Then the page is redirected to customer dashboard
+    Given user fill sign in form with new data
+    When user clicks create account button
+    Then successful login
     When user go to change password page
     And user fill password change form with new password different than repeat password
     Then both password must match message must be displayed
