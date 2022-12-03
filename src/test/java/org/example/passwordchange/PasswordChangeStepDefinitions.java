eafbda44365e282c7b4471901691b290a953a9c3
Luís Filipe Soutello <lfsoutello@gmail.com>
2022-12-03 17:02:19 -0300

    feat: password change success

--- src/test/java/org/example/passwordchange/PasswordChangeStepDefinitions.java
+++ src/test/java/org/example/passwordchange/PasswordChangeStepDefinitions.java
@@ -4,6 +4,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -15,10 +16,8 @@ public class PasswordChangeStepDefinitions {
 
     private WebDriver driver;
 
-    @Given("user fill form with new password")
+    @When("user fill password change form")
     public void fillChangePasswordForm() {
-        driver.get("http://multibags.1dt.com.br/shop/customer/password.html");
-
         WebElement currentPassword = driver.findElement(By.id("currentPassword"));
         WebElement newPassword = driver.findElement(By.id("password"));
         WebElement repeatPassword = driver.findElement(By.id("checkPassword"));
@@ -28,44 +27,18 @@ public class PasswordChangeStepDefinitions {
         repeatPassword.sendKeys("098765");
     }
 
-    @Given("user log in")
-    public void logIn() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.get("http://multibags.1dt.com.br/shop/customer/customLogon.html");
-
-        WebElement email = driver.findElement(By.id("signin_userName"));
-        WebElement password = driver.findElement(By.id("signin_password"));
-
-        email.sendKeys("change@password.com");
-        password.sendKeys("123456");
-
-        WebElement loginButton = driver.findElement(By.id("genericLogin-button"));
-        loginButton.click();
-
+    @When("user go to change password page")
+    public void goToChangePassword() {
+        driver.get("http://multibags.1dt.com.br/shop/customer/password.html");
         new WebDriverWait(driver,10L).until(new ExpectedCondition<Boolean>() {
             public Boolean apply(WebDriver d) {
-                return d.getCurrentUrl().startsWith("http://multibags.1dt.com.br/shop/customer/dashboard.html");
+                return d.findElement(By.id("formError")).isDisplayed();
             }
         });
     }
 
     @When("user clicks change password")
     public void clickChangePassword() {
-        WebElement loginButton = driver.findElement(By.id("submitChangePassword"));
-        loginButton.click();
-    }
-
-    @When("user changes password back to old")
-    public void changePasswordBackToOld() {
-        WebElement currentPassword = driver.findElement(By.id("currentPassword"));
-        WebElement newPassword = driver.findElement(By.id("password"));
-        WebElement repeatPassword = driver.findElement(By.id("checkPassword"));
-
-        currentPassword.sendKeys("098765");
-        newPassword.sendKeys("123456");
-        repeatPassword.sendKeys("123456");
-
         WebElement loginButton = driver.findElement(By.id("submitChangePassword"));
         loginButton.click();
     }
@@ -78,5 +51,43 @@ public class PasswordChangeStepDefinitions {
             }
         });
         driver.quit();
+    }
+
+    @Given("user fill form with new data")
+    public void fillFormNewData() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("http://multibags.1dt.com.br/shop/customer/registration.html");
+
+        WebElement firstName = driver.findElement(By.id("firstName"));
+        WebElement lastName = driver.findElement(By.id("lastName"));
+        WebElement state = driver.findElement(By.id("hidden_zones"));
+        WebElement email = driver.findElement(By.id("emailAddress"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement repeatPassword = driver.findElement(By.id("passwordAgain"));
+
+        String randomString = RandomStringUtils.randomAlphabetic(4);
+
+        firstName.sendKeys("Seu Zé ");
+        lastName.sendKeys("Do Teste");
+        state.sendKeys("São Paulo");
+        email.sendKeys(randomString + "seuze@test.com");
+        password.sendKeys("123456");
+        repeatPassword.sendKeys("123456");
+    }
+
+    @When("user clicks create account")
+    public void clickCreateAccount() {
+        WebElement createAccountButton = driver.findElement(By.className("login-btn"));
+        createAccountButton.click();
+    }
+
+    @Then("the page is redirected to customer dashboard")
+    public void pageRedirect() {
+        new WebDriverWait(driver,10L).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getCurrentUrl().startsWith("http://multibags.1dt.com.br/shop/customer/dashboard.html");
+            }
+        });
     }
 }
--- src/test/resources/org/example/passwordchange/passwordchange.feature
+++ src/test/resources/org/example/passwordchange/passwordchange.feature
@@ -1,9 +1,10 @@
 Feature: Change Password
 
   Scenario: Successful change password
-    Given user log in
-    And user fill form with new password
-    When user clicks change password
-    Then change password success message is displayed
-    When user changes password back to old
+    Given user fill form with new data
+    When user clicks create account
+    Then the page is redirected to customer dashboard
+    When user go to change password page
+    And user fill password change form
+    And user clicks change password
     Then change password success message is displayed
